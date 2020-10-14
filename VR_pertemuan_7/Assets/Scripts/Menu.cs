caec605d2660f10b7b123f3c38a0e9b748df3e08
AgusSubaktiar <43577605+AgusSubaktiar@users.noreply.github.com>
2020-10-14 13:43:38 +0700

    Finish Praktikum dan Tugas

---
+++ VR_pertemuan_7/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   
+   public void pindahHalaman(string halaman)
+   {
+    
+       SceneManager.LoadScene(halaman);
+   }
+
+   public void keluarAplikasi()
+   {
+       Application.Quit();
+   }
+}
